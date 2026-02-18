f9927329e37b3aff17d374bcd07ec9fa5effff16
Alihar2004 <aliharkebus@gmail.com>
2026-02-18 23:02:48 +0800

    Create Lab1-9.py

---
+++ LAB 1/Lab1-9.py
@@ -0,0 +1,9 @@
+a = float (input("Value of A: "))# input a float value for variable a here
+b = float (input("Value of B: "))# input a float value for variable b here
+
+print(a+b)# output the result of addition here
+print(a-b)# output the result of subtraction here
+print(a*b)# output the result of multiplication here
+print(a/b)# output the result of division here
+
+print("\nThat's all, folks!")
